89cf3f36b2c954ed27e09bfb43eaf6e4b449558a
kotryn <kotryn1415@gmail.com>
2017-06-21 00:50:09 +0200

    Get login from attribute

--- 2-github-profile-card/github-profile-card-element/main.js
+++ 2-github-profile-card/github-profile-card-element/main.js
@@ -1,112 +1,60 @@
-/*class MockupElement extends HTMLElement {
- constructor(){
- super();
- console.log('new MockapElement');
- this.shadow = this.attachShadow({mode: 'open'});
- console.log(this.shadow);
- }
-
- connectedCallback(){
- console.log('MockapElement#connectedCallback');
-
- this.shadow.innerHTML = document.currentScript.ownerDocument.querySelector('template').innerHTML;
-
- this.shadow.querySelector('img').setAttribute('src',this.attributes.image.value);
- this.shadow.querySelector('h1').innerHTML = this.attributes.label.value;
-
- }
- }
-
- window.customElements.define('mockup-element', MockupElement);*/
-
 class GithubMockupElement extends HTMLElement {
     constructor() {
         super();
-        console.log('git');
         this.shadow = this.attachShadow({mode: 'open'});
-        console.log(this.shadow);
     }
 
     connectedCallback() {
-        console.log('gitttt');
 
         this.shadow.innerHTML = document.currentScript.ownerDocument.querySelector('template').innerHTML;
+        let login = this.attributes.login.value;
 
-
-
-        //this.shadow.querySelector('h1').innerHTML = this.attributes.label.value;
-
-
-        this._fetch_github_profile();
-        this._fetch_github_profile_2();
-
-
-       // this.shadow.querySelector('img').setAttribute('src', );
-
-
+        this._fetch_github_profile(login);
+        this._fetch_github_profile_2(login);
     }
 
-    _fetch_github_profile() {
-        var request = new Request('./github-profile-card-element/mocks/github-piecioshka-profile.json', {
-            /*headers: new Headers({
-             'Content-Type': 'text/plain'
-             })*/
-        });
-
+    _fetch_github_profile(login) {
+        let request = new Request('/mocks/github-piecioshka-profile.json', {});
+        //let request = new Request('https://api.github.com/users/'+login, {});
         return fetch(request)
 
             .then((response) => {
                 response = response.json();
-                console.log(response);
                 return response;
             })
 
             .then((data) => {
-                console.log('........');
-                console.log(data.avatar_url);
 
                 this.shadow.querySelector('img').setAttribute('src', data.avatar_url);
                 this.shadow.querySelector('.name').innerHTML = data.name;
                 this.shadow.querySelector('.bio').innerHTML = data.bio;
                 this.shadow.querySelector('.location').innerHTML = data.location;
-
-
-                return data;
             });
     }
 
-    _fetch_github_profile_2() {
-        var request = new Request('./github-profile-card-element/mocks/github-piecioshka-repositories.json', {
-            /*headers: new Headers({
-             'Content-Type': 'text/plain'
-             })*/
-        });
+    _fetch_github_profile_2(login) {
+        let request = new Request('/mocks/github-piecioshka-repositories.json', { });
+        //let request = new Request('https://api.github.com/users/'+login+'/repos', { });
+
 
         return fetch(request)
 
             .then((response) => {
                 response = response.json();
-                console.log(response);
                 return response;
             })
 
             .then((data) => {
-                console.log('........');
-                console.log(data[0].stargazers_count);
 
                 this.shadow.querySelector('.star').innerHTML = this._github_star(data);
-
-
-                return data;
             });
     }
 
     _github_star(data){
         let temp = '';
-        //let sortedData = data.cloneNode();
         data.sort(function(a,b) {return (a.stargazers_count < b.stargazers_count) ? 1 : ((b.stargazers_count < a.stargazers_count) ? -1 : 0);} );
 
-        for (let i = 0; i < 9; i++){
+        for (let i = 0; i < 9 && i < data.length; i++){
             temp =  temp + '<li class="star_image">'+data[i].stargazers_count + ' ☆ </li><li>' + data[i].name + '</li>';
         }
         return temp;
